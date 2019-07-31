eaaec69855431ba6e94794139b3e2b6cc7d0887c
Anthony Favier <favier-anthony@hotmail.fr>
2019-07-31 11:00:45 +0200

    plop

--- bag.sh
+++ bag.sh
@@ -1,2 +1,2 @@
-rosbag record /mission /hmi_done /summit_xl_a/robotnik_base_control/cmd_vel /pose /clock
+rosbag record /mission /hmi_done /summit_xl_a/robotnik_base_control/cmd_vel /pose /clock /do_controlled_stop /do_switch_to_teleop
 
